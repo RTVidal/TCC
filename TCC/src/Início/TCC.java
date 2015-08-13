a76b373e8026f8fb4e789c07aeaf2440259a4a23
RTVidal <rafaelvidal4@gmail.com>
2015-08-12 22:56:36 -0300

    ...

--- TCC/src/Início/TCC.java
+++ TCC/src/Início/TCC.java
@@ -33,68 +33,7 @@ public class TCC {
             JanelaInicial ji = new JanelaInicial();
             ji.setVisible(true);
 
-            //Preenche a instancia do assistente
-            Assistente assistente = new Assistente();
-
-            assistente.setNome("Joselito");
-            assistente.setApresentacao("Oi amiguinho");
-            assistente.setAvatarAssistente("avatar1");
-
-            Assistente.setInstancia(assistente);
-
-            Situacao situacao = new Situacao();
-
-            File file = new File("./recursos/teste.jpg");
-            BufferedImage img = ImageIO.read(file);
-
-            situacao.setFundoSituacao(img);
-            situacao.setFalaAssistente("No menu está uma variedade de sanduíches com preparos, "
-                    + "apresentações e acompanhamentos criativos. Há hambúrguer de "
-                    + "kafta, joelho de porco, falafel, feijoada, barreado, "
-                    + "couve-flor e diversos cortes de carnes, inclusive premium. "
-                    + "Nos pães também há grande variedade. Tem pão folha, "
-                    + "integral, de aveia, batata, milho, brioche e de aipim, "
-                    + "apenas para citar alguns. Os acompanhamentos são outras "
-                    + "atrações. Além da tradicional batata frita, há guacamole, "
-                    + "nuggets de banana, palmito assado, minipastel de geleia de "
-                    + "pimenta, babaganoush e chips de banana.");
-
-            Saida saida1 = new Saida();
-            saida1.setNome("Saida 1");
-            saida1.setFalaAssistente("Você escolheu saida 1");
-
-            Saida saida2 = new Saida();
-            saida2.setNome("Saida 2");
-            saida2.setFalaAssistente("É a saída 2 que você escolheu");
-
-            Saida saida3 = new Saida();
-            saida3.setNome("Saída 3");
-            saida3.setFalaAssistente("Você quer a saída 3?");
-
-            Saida saida4 = new Saida();
-            saida4.setNome("Saida 4");
-            saida4.setFalaAssistente("Saída 4 então");
-
-            ArrayList<Saida> saidas = new ArrayList<>();
-
-            saidas.add(saida1);
-            saidas.add(saida2);
-            saidas.add(saida3);
-            saidas.add(saida4);
-
-            situacao.setSaidas(saidas);
-
-        //Grava a situação
-//        IOPartida iop = new IOPartida();
-//        iop.SalvaSituacao(situacao);
-//        
-//        //Recupera a situação
-//        //IOPartida iop = new IOPartida();
-//        Situacao situacao2 = iop.LeSituacao();
-            JanelaSituacaoJogo jsj = new JanelaSituacaoJogo(situacao, assistente);
-
-            jsj.setVisible(true);
-            jsj.setSize(1024, 768);
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
