1b44d305932617ef1d10fdc19a8b51178397f8b2
RTVidal <rafaelvidal4@gmail.com>
2015-09-29 20:58:49 -0300

    inicio to dentro início to fora

--- TCC/src/Inicio/TCC.java
+++ TCC/src/Inicio/TCC.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Início;
+package Inicio;
 
 import GUI.Jogador.JanelaInicial;
 import javax.swing.JOptionPane;
